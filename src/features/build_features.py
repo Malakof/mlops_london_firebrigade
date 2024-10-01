83dfd0d76ea273c2d773ec37306ce25797d49c2f
Richard De Falco <Malakof@users.noreply.github.com>
2024-10-01 09:04:31 +0200

    Distance correction

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -24,7 +24,7 @@ def clean_data(df_incident, df_mobilisation, df_stations):
                               'DelayCode_Description',
                               'TurnoutTimeSeconds', 'TravelTimeSeconds', 'DeployedFromLocation', 'BoroughName',
                               'WardName']
-        # , 'name', 'address', 'borough']
+
     df_mobilisation.drop(drop_cols_mobilisation, axis=1, inplace=True)
     df_mobilisation = pd.merge(df_mobilisation, df_stations, left_on='DeployedFromStation_Name', right_on='name_station', how='left')
     df_mobilisation = df_mobilisation.drop(['name_station', 'address_station', 'borough_station'], axis=1)
@@ -42,11 +42,12 @@ def clean_data(df_incident, df_mobilisation, df_stations):
                           'Notional Cost (£)', 'NumCalls', 'IncidentStationGround']
                           #changement de nom de colonne
     df_incident.drop(drop_cols_incident, axis=1, inplace=True)
+    df_incident = df_incident[df_incident['Latitude'] != 0]
 
     # Convert British National Grid to WGS84
     bng = 'epsg:27700'
     wgs84 = 'epsg:4326'
-    transformer = Transformer.from_crs(bng, wgs84, always_xy=True)
+    transformer = Transformer.from_crs(bng, wgs84, always_xy=False)
     df_incident[['Latitude', 'Longitude']] = df_incident.apply(
         lambda row: transformer.transform(row['Easting_rounded'], row['Northing_rounded']), axis=1,
         result_type='expand')
@@ -59,21 +60,20 @@ def merge_datasets(df_incident, df_mobilisation):
     df_merged = pd.merge(df_incident, df_mobilisation, on='IncidentNumber', how='left')
     df_merged.dropna(inplace=True)
     df_merged['distance'] = df_merged.apply(
-        lambda row: haversine((row.latitude_station, row.longitude_station),
-                              (row.Latitude, row.Longitude), unit=Unit.KILOMETERS), axis=1)
+         lambda row: haversine((row.latitude_station, row.longitude_station),
+                               (row.Latitude, row.Longitude), unit=Unit.KILOMETERS), axis=1)
     df_merged['VitesseMoy'] = df_merged['distance'] / (df_merged['AttendanceTimeSeconds'] / 3600)
-    # df_merged = df_merged[(df_merged['AttendanceTimeSeconds'] >= cfg.BandWidth_AttendanceTimeSeconds_min) & (
-    #             df_merged['AttendanceTimeSeconds'] <= cfg.BandWidth_AttendanceTimeSeconds_max)]
-    # df_merged = df_merged[
-    #     (df_merged['VitesseMoy'] >= cfg.BandWidth_speed_min) & (df_merged['VitesseMoy'] <= cfg.BandWidth_speed_max)]
+    df_merged = df_merged[(df_merged['AttendanceTimeSeconds'] >= cfg.BandWidth_AttendanceTimeSeconds_min) & (
+                df_merged['AttendanceTimeSeconds'] <= cfg.BandWidth_AttendanceTimeSeconds_max)]
+    df_merged = df_merged[
+        (df_merged['VitesseMoy'] >= cfg.BandWidth_speed_min) & (df_merged['VitesseMoy'] <= cfg.BandWidth_speed_max)]
 
     df_merged = df_merged.drop(['IncidentNumber', 'DateAndTimeMobilised', 'DeployedFromStation_Code',
-                                'latitude_station', 'longitude_station',
+                                'latitude_station', 'longitude_station', 'Latitude', 'Longitude',
                                 'Resource_Code',
                                 'VitesseMoy'
                                 ], axis=1)
     return df_merged
-
 
 def save_data(df_merged):
     df_merged.to_csv(str(os.path.join(cfg.chemin_data, cfg.fichier_global)), index=False)
