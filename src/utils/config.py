a9f24153b984f301749247044d89775b86f057e9
Vinc54 <vincent.dosreis@orange.com>
2024-11-21 21:35:07 +0000

    Correctif download fichier ajout docker mlflow

--- src/utils/config.py
+++ src/utils/config.py
@@ -25,7 +25,10 @@ if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
 # définition des urls
-url_incident = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20from%202009%20-%202017.csv"
+#url_incident = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20from%202009%20-%202017.csv"
+url_incident = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/f5066d66-c7a3-415f-9629-026fbda61822/LFB%20Incident%20data%20from%202018%20onwards.csv.xlsx"
+
+#url_mobilisation = "https://data.london.gov.uk/download/london-fire-brigade-mobilisation-records/3ff29fb5-3935-41b2-89f1-38571059237e/LFB%20Mobilisation%20data%202021%20-%202024.xlsx"
 url_mobilisation = "https://data.london.gov.uk/download/london-fire-brigade-mobilisation-records/3ff29fb5-3935-41b2-89f1-38571059237e/LFB%20Mobilisation%20data%202021%20-%202024.xlsx"
 
 # Définition des nom de fichier
@@ -83,7 +86,7 @@ PUSH_GETAWAY_ENABLED = False
 PUSHGATEWAY_URL = 'http://localhost:9091'
 
 USE_MLFLOW = False  # This can be toggled to enable/disable MLflow integration
-MLFLOW_TRACKING_URI = 'http://localhost:9092'  # URI to MLflow tracking server
+MLFLOW_TRACKING_URI = 'http://mlflow_service:9092'  # URI to MLflow tracking server
 MLFLOW_EXPERIMENT_NAME = 'LFB_MLOPS'  # Name of the MLflow experiment
 
 # Metrics definitions
