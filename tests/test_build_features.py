ca538cff6eecc9e8a037481107d0f63a25b3b1ae
Richard De Falco <Malakof@users.noreply.github.com>
2024-11-26 13:45:53 +0100

    Update test_build_features.py

--- tests/test_build_features.py
+++ tests/test_build_features.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 import unittest
-from src.features import build_features
+# from src.features import build_features
 
 class TestBuildFeatures(unittest.TestCase):
 
