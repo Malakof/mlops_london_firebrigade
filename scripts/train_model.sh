ffaba9334b5fde1f8b8e63536d99cbeb72217ad5
Richard De Falco <Malakof@users.noreply.github.com>
2024-12-03 13:54:48 +0100

    Commented train_model.sh

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -3,18 +3,18 @@
 # Fichier shell pour tester l'API FastAPI
 
 test_health() {
-  echo "Test Health check..."
+  echo "--> Health check..."
   curl -X GET "http://127.0.0.1:8000/health"
   echo -e "\n"
 }
 
 test_predict() {
-  echo "Test Predict..."
+  echo "--> Predict..."
   curl 'http://localhost:8000/predict?distance=2.5&station=Acton'
 }
 
 test_data_preprocessing() {
-  echo "Test process full..."
+  echo "--> Data download and processing full..."
   curl -X 'GET' \
   'http://127.0.0.1:8000/process_data?incident=true&mobilisation=true&convert_to_pickle=false' \
   -u 'admin:fireforce' \
@@ -23,7 +23,7 @@ test_data_preprocessing() {
 }
 
 test_build_features() {
-  echo "Test process full..."
+  echo "--> Build Features..."
 
   curl -X 'GET' \
     'http://127.0.0.1:8000/build_features' \
@@ -34,7 +34,7 @@ test_build_features() {
 }
 
 test_train_model_mlflow_docker() {
-  echo "Test train with model in mlflow in docker..."
+  echo "--> Train with prod model from mlflow..."
 
   curl -X 'POST' \
     'http://127.0.0.1:8000/train_model' \
